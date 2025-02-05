17a9b50c6ac6089b3cb3314d060ca16c811ac5ac
squid <squid@stormwind.it>
2025-02-05 10:16:09 +0100

    feat: added plugins che mi servono

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,21 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
+  { import = "astrocommunity.lsp.nvim-java" },
+  { import = "astrocommunity.pack.cs-omnisharp" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.recipes.vscode-icons" },
+  { import = "astrocommunity.split-and-window.minimap-vim" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+
   -- import/override with your plugins folder
 }
