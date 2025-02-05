cdebabb81fb69f25e9551e0c063e4dff69990725
squid <squid@stormwind.it>
2025-02-05 09:48:25 +0100

    chore(astrolsp.lua): comment out unnecessary line to improve code readability chore(mason.lua): add "omnisharp" to the list of language servers for installation chore(user.lua): comment out unnecessary line to improve code readability and update dashboard header customization with ASCII art

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -11,6 +11,7 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
+        "omnisharp",
         -- add more arguments for adding more language servers
       },
     },
